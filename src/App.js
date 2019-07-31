882f887673f8bf0b21ff70756412e8c1ae329cd6
geraldMaboshe <maboshegerald1@gmail.com>
2019-07-31 15:57:32 +0200

    updated homepage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import './App.css';
-import Academic from './components/Academic';
-import Languages from './components/Languages';
-import Photo from './components/Photo';
-import SoftSkills from './components/SoftSkils';
-import Learn from './components/Learn';
-import Hobbies from './components/Hobbies';
-import Fears from './components/Fears';
-
+import Events from './components/Events';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Academic />
-        <Languages />
-        <Photo />
-        <SoftSkills />
-        <Learn />
-        <Hobbies />
-        <Fears />
-        <p> &copy; 2019 Gerald Maboshe - Bongohive Internship Program </p>
-      </div>
+      <Router>
+     <div className="mainDiv">
+     
+       <Route exact path="/" render={props =>(
+         <>
+         <Header />
+        
+         </>
+       )}>
+       
+       </Route>
+       <Route path="/events" component={Events}>
+
+       </Route>
+       
+     </div>
+     </Router>
+   
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,87 @@
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom';
 class Header extends Component {
 
     render() {
         return (
             <div style={styling}>
-                <p>My Portfolio</p>
+                <div style={navigation}>
+                    <Link style={Link1} to="/">🖥 Gerald Maboshe</Link> 
+                    <div style={divLink}>
+                        <Link style={otherLinks}  to="articles">Articles</Link> | <Link style={otherLinks} to="events">Events</Link>
+                    </div>
+                </div>
+                <div style={main}>
+                    <h1>Hey, I'm Gerald 😊
+                    <img style={imgStyle} src="pic.jpg"/>
+                    </h1>
+                        <p>
+                        I'm a modern JavaScript developer and a data science enthusiast <br />
+                        I love building web apps and everything tech
+                    </p>
+                </div>
+                <div style={followBtns}>
+                    <button style={btns}> 
+                        <img src="github.png" width="20px"/>
+                        Follow
+                    </button>
+                    <button style={btns}> 
+                        <img src="linked.png" width="20px"/>
+                        Connect
+                    </button>
+                    <button style={btns}> 
+                        <img src="twitter.png" width="20px"/>
+                        Follow
+                    </button>
+                </div>
+            
             </div>
         );
     }
 }
 const styling = {
-    background: '#333',
-    padding: "35px",
-    textAlign: 'center',
-    fontSize: '50px',
-    color: '#ff9900'
+    //backgroundColor: "#4682B4", 
+    //height: "500px"  
+    //float:"left"
 }
+const main = {
+    marginTop: "150px",
+    marginLeft: "150px",
+    marginRight: "150px",
+    
+    
+}
+const imgStyle = {
+    float: "right",
+    borderRadius: "50%",
+    width: '200px', 
+}
+
+const Link1 = {
+    textDecoration: "none",
+    marginLeft: "40px",
+    fontSize: "30px",
+}
+ const divLink = {
+   float:'right'
+ }
+ const otherLinks = {
+    textDecoration:"none"
+ }
+ const navigation = {
+    marginTop:'40px',
+    marginRight:'40px'
+ }
+
+ const followBtns = {
+     marginLeft:"150px",
+     paddingTop:"40px",
+    
+ }  
+ const btns = {
+    textAlign: 'center',
+    width:"80px",
+    margin: "4px 2px",
+    borderRadius:"2px"
+ }
 export default Header;
